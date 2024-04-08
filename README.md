8d7f4184cf4f974e20e269bdaabeb7effb4440ad
Darrens24 <edouard.leleux@gmail.com>
2024-04-08 19:34:12 +0200

    installations, README, gitignore

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # Ahctam
 Teem
+
+## Flask
+### Requirements
+Having at least python3.10 available
+Install postgresql
+#### On debian based/Ubuntu:
+```
+sudo apt install postgresql postgresql-contrib libpq-dev
+sudo apt install python3-dev build-essential
+```
+To test it
+```
+sudo -u postgres psql
+```
+
+To install and start the venv
+```
+source init.sh
+```
+To desactivate it
+```
+deactivate
+```
---
+++ init.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# On debian based/Ubuntu:
+# sudo apt install postgresql postgresql-contrib libpq-dev
+# sudo systemctl postgresql start
+# sudo -u postgres psql
+# sudo apt install python3-dev build-essential
+
+# To automatically activate the venv after the script, run source init.sh
+VENV="matcha_venv"
+
+python3 -m venv $VENV
+source $VENV/bin/activate
+
+if [ ! -f .gitignore ]; then
+    touch .gitignore
+    echo ".gitignore created"
+fi
+if ! grep -q "__pycache__/" .gitignore; then
+    echo "__pycache__/" >> .gitignore
+    echo "pycache folders added to .gitignore"
+fi
+if ! grep -q $VENV .gitignore; then
+    echo $VENV >> .gitignore
+    echo "$VENV added to .gitignore"
+fi
+if [ ! -f requirements.txt ]; then
+    echo "requirements.txt not found"
+    return
+fi
+
+pip install -r requirements.txt
+if [ $? -ne 0 ]; then
+    echo "An error occurred while installing the dependencies."
+    return
+  else
+    echo "Dependencies installed successfully."
+fi
