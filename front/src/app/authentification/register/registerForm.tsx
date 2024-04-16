5b35f5a66f12d89c9ec7ee7481dde9e6d8955d80
Timothée BRANDT <brandt.timothee.42nice@gmail.com>
2024-04-16 20:51:24 +0200

    another dynamic layout

--- front/src/app/authentification/register/registerForm.tsx
+++ front/src/app/authentification/register/registerForm.tsx
@@ -2,46 +2,18 @@
 
 import React from 'react';
 import { useEffect, useState } from 'react';
-import './authentification.scss'
+import './registerForm.scss'
 import Link from 'next/link';
-import { State } from '../types';
-import { useAuth } from '../authContext';
-import Button from '../components/button';
+import { State } from '../../types';
+import { useAuth } from '../../authContext';
+import Button from '../../components/button';
 
-const Authentification: React.FC = () => {
+const RegisterForm: React.FC = () => {
 	const {login, logout, isJwtInCookie, user, setUser } = useAuth();
 	const [email, setEmail] = useState('');
 	const [password, setPassword] = useState('');
 	const [ status, setStatus ] = useState<State>(State.initial);
 	const [ isBadCredentials, setIsBadCredentials] = useState<boolean>(false);
-
-	useEffect(() => {
-		if (status == State.initial && !user?.userName && isJwtInCookie('jwtToken')) {
-			// CALL ENDPOINT TO GET THE USER INFORMATIONS
-			// GET METHOD WITH THE JWT IN HEADER
-			// FOR NOW myPIG IS A MOCK :D
-			const myPig = {
-				id: 12,
-				userName: 'Julie',
-				firstName: 'Brandt',
-				lastName: 'Juju',
-				email: email,
-				password_hash: password,
-				is_active: true,
-				registration_token: 'qwertyuiop',
-				jwt_token: '123456789asdsfgbvncxvbn',
-				gender: 'female',
-				sexual_preferences: 'male',
-				biography: 'osef',
-				interests: 'osef',
-				created_at: '1234567876543',
-				firstTimeLogged: false,
-			}
-			setUser(myPig);
-			login(myPig);
-		}
-		setStatus(State.done)
-	  }, []);
 	
 	const submit = async (event: any) => {
 		event.preventDefault();
@@ -100,8 +72,6 @@ const Authentification: React.FC = () => {
 		};
   return (
 	<>
-		{isBadCredentials && <p className="bad-credentials">Bad credentials</p>}
-		{!user.firstName && status != State.initial ? (
 		<div className="form-container">
 			<form onSubmit={submit} className="form">
 				<div>
@@ -126,26 +96,17 @@ const Authentification: React.FC = () => {
 						autoComplete="new-password"
 					/>
 				</div>
-				<Button title="Log in" type="submit" onClick={() => {}}/>
+				<Button title="Register" type="submit" onClick={() => {}}/>
 					<div className="new_member">
-						<p className="new_member-question">Not a member yet ?</p>
-						<Link className="new_member-creation" href="/register">
-							Create an account
+						<p className="new_member-question">Already have an Account ?</p>
+						<Link className="new_member-creation" href="/">
+							Login!
 						</Link>
 					</div>
 			</form>
 		</div>
-		) : (
-			<>
-				{user?.firstName && <div className="user-card">
-					<p>Welcome {user?.firstName}</p>
-					<img className="profile-picture" src="https://cdn4.volusion.store/kapts-nrbqf/v/vspfiles/photos/GUINEAPIGONEDRESSED-2.jpg?v-cache=1590745950"></img>
-					<Button title="Logout" className="button-logout" onClick={logout} />
-				</div>}
-			</>
-		)}
 	</>
   );
 }
 
-export default Authentification;
+export default RegisterForm;
