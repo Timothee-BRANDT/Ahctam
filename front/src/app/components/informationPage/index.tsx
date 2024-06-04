2d989a6939a49c974e4e5537b81f70dd49d7cf3c
Timothée BRANDT <timothee@metacard.gift>
2024-06-04 11:51:25 +0200

    ?????

--- front/src/app/components/informationPage/index.tsx
+++ front/src/app/components/informationPage/index.tsx
@@ -4,6 +4,8 @@ import './index.scss';
 import Button from '../core/button/button';
 import { serverIP } from '@/app/constants';
 
+const CLASSNAME = 'profile';
+
 const InformationPage: React.FC = () => {
   const [profile, setProfile] = useState({
     gender: '',
@@ -15,6 +17,8 @@ const InformationPage: React.FC = () => {
 
   const handleChange = (e: any) => {
     const { name, value } = e.target;
+    console.log(name)
+    console.log(name)
     setProfile({
       ...profile,
       [name]: value,
@@ -58,59 +62,93 @@ const InformationPage: React.FC = () => {
 
   return (
     <>
-      <div className="profile-container">
+      <div className={CLASSNAME}>
         <form onSubmit={handleSubmit}>
-          <label htmlFor="gender">Gender</label>
-          <select name="gender" value={profile.gender} onChange={handleChange} required>
-            <option value="" disabled>Select Gender</option>
-            <option value="female">Female</option>
-            <option value="male">Male</option>
-            <option value="non-binary">Non-binary</option>
-          </select>
+            <p className={`${CLASSNAME}__title`}>I am</p>
+            <div className={`${CLASSNAME}__sexual-preference`}>
+                <div>
+                    <input className={`${CLASSNAME}__sexual-preference--input`} type="radio" 
+                        id="gender-female" name="gender" value="female" 
+                        checked={profile.gender === 'female'} 
+                        onChange={handleChange} required />
+                    <label htmlFor="gender-female">Female</label>
+                </div>
+                <div>
+                    <input className={`${CLASSNAME}__sexual-preference--input`} type="radio" 
+                        id="gender-male" name="gender" value="male"
+                        checked={profile.gender === 'male'}
+                        onChange={handleChange} required />
+                    <label htmlFor="gender-male">Male</label>
+                </div>
+                <div>
+                    <input className={`${CLASSNAME}__sexual-preference--input`} type="radio"
+                        id="gender-non-binary" name="gender" value="non-binary"
+                        checked={profile.gender === 'non-binary'}
+                        onChange={handleChange} required />
+                    <label htmlFor="gender-non-binary">Non-binary</label>
+                </div>
+            </div>
 
-          <label htmlFor="sexualPreference">Sexual Preference</label>
-          <input
-            type="text"
-            name="sexualPreference"
-            value={profile.sexualPreference}
-            onChange={handleChange}
-          />
+            <p className={`${CLASSNAME}__title`}>I'm looking for</p>
+            <div className={`${CLASSNAME}__sexual-preference`}>
+                <div>
+                    <input className={`${CLASSNAME}__sexual-preference--input`} type="radio" 
+                        id="female" name="sexualPreference" value="female" 
+                        checked={profile.sexualPreference === 'female'} 
+                        onChange={handleChange} required />
+                    <label htmlFor="sexual-female">Female</label>
+                </div>
+                <div>
+                    <input className={`${CLASSNAME}__sexual-preference--input`} type="radio" 
+                        id="male" name="sexualPreference" value="male"
+                        checked={profile.sexualPreference === 'male'}
+                        onChange={handleChange} required />
+                    <label htmlFor="sexual-male">Male</label>
+                </div>
+                <div>
+                    <input className={`${CLASSNAME}__sexual-preference--input`} type="radio"
+                        id="both" name="sexualPreference" value="both"
+                        checked={profile.sexualPreference === 'both'}
+                        onChange={handleChange} required />
+                    <label htmlFor="sexual-both">Both</label>
+                </div>
+            </div>
 
-          <label htmlFor="biography">Biography</label>
-          <textarea
-            name="biography"
-            value={profile.biography}
-            onChange={handleChange}
-          />
+            <p className={`${CLASSNAME}__title`}>About me</p>
+            <textarea
+                name="biography"
+                value={profile.biography}
+                onChange={handleChange}
+            />
 
-          <label htmlFor="interests">Interests</label>
-          <input
-            type="text"
-            name="interests"
-            value={profile.interests}
-            onChange={handleChange}
-            placeholder="#hay #vegetables #pellets"
-          />
+            <p className={`${CLASSNAME}__title`}>Interests</p>
+            <input
+                type="text"
+                name="interests"
+                value={profile.interests}
+                onChange={handleChange}
+                placeholder="#hay #vegetables #pellets"
+            />
 
-          <div className="photo-upload-container">
-            {profile.photos.map((photo, index) => (
-              <div onClick={() => document.getElementsByName(`photoUpload${index}`)[0].click()} key={index} className="photo-placeholder" style={{ backgroundImage: photo ? `url(${photo})` : 'none' }}>
-                <input
-                  type="file"
-                  name={`photoUpload${index}`}
-                  accept="image/*"
-                  onChange={(e) => handleImageChange(index, e)}
-                  style={{ display: 'none' }}
-                />
-                {!photo ? <div className="upload-text">Upload a picture</div>: ''}
-              </div>
-            ))}
-          </div>
-          <Button className="button-info" title="Save" type="submit" onClick={() => {}} />
+            <div className="photo-upload-container">
+                {profile.photos.map((photo, index) => (
+                <div onClick={() => document.getElementsByName(`photoUpload${index}`)[0].click()}
+                    key={index} className="photo-placeholder" 
+                    style={{ backgroundImage: photo ? `url(${photo})` : 'none' }}>
+                    <input
+                    type="file"
+                    name={`photoUpload${index}`}
+                    accept="image/*"
+                    onChange={(e) => handleImageChange(index, e)}
+                    style={{ display: 'none' }}
+                    />
+                    {!photo ? <div className="upload-text">Upload a picture</div>: ''}
+                </div>
+                ))}
+            </div>
+            <Button className="button-info" title="Save" type="submit" onClick={() => {}} />
         </form>
       </div>
     </>
   );
 }
-
-export default InformationPage;
