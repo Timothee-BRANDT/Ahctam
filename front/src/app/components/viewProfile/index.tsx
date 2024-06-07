3538f0eedb63844f431233c6c3ea539c498789de
Timothée BRANDT <timothee@metacard.gift>
2024-06-07 11:48:17 +0200

    mock data for user photos

--- front/src/app/components/viewProfile/index.tsx
+++ front/src/app/components/viewProfile/index.tsx
@@ -15,23 +15,53 @@ import {
 } from "@/components/ui/carousel"
 
 import './index.scss';
+import { serverIP } from '@/app/constants';
+
+interface ProfileViewProps {
+    id: string | string[];
+}
 
 const CLASSNAME = 'carousel';
 
-const ProfileView: React.FC = () => {
-    const { user } = useAuth();
+const ProfileView: React.FC<ProfileViewProps> = (id) => {
+
+    // const getUser = async () => {
+    //     try {
+    //         const response = await fetch(`http://${serverIP}:5000/getUser/${id}`, {
+    //             method: 'GET',
+    //             headers: {
+    //                 'Content-Type': 'application/json'
+    //             },
+    //         });
+
+    //         if (!response.ok) {
+    //             throw new Error('Network response was not ok');
+    //         }
+    //         const data = await response.json();
+    //         console.log('Server response:', data);
+    //     } catch (e) {
+    //         console.log(e);
+    //     }
+    // }
+
+    const user = data.user; // mock
+    // should be the const user = response.data;
+
+    // useEffect(() => {
+    //     getUser();
+    // })
 
     return (
         <>
             <div className="carousel">
                 <Carousel className="w-full max-w-xs">
                     <CarouselContent>
-                        {Array.from({ length: 6 }).map((_, index) => (
+                        {user.photos.map((photo, index) => (
                             <CarouselItem key={index}>
                                 <div className="p-1">
                                     <Card>
                                         <CardContent className="flex aspect-square items-center justify-center p-6 custo">
-                                            <img className={`${CLASSNAME}__image`} src={data.user.photos[index]} alt='' />
+                                            <img className={`${CLASSNAME}__image`} src={photo} alt='user-photo' />
                                         </CardContent>
                                     </Card>
                                 </div>
