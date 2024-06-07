417081d3a012a0c7858126191202f12a648d7acf
Timothée BRANDT <timothee@metacard.gift>
2024-06-07 08:36:56 +0200

    like/dislike buttons, redirect when click on usercard

--- front/src/app/components/core/user/userCard.tsx
+++ front/src/app/components/core/user/userCard.tsx
@@ -8,28 +8,41 @@ import "./userCard.scss";
 interface UserCardProps {
     user: User,
     className?: string,
+    liked: () => void;
+    disliked: () => void;
+    redirect: () => void;
 }
 
 const CLASSNAME = 'card';
 
-const UserCard: React.FC<UserCardProps> = ({ user, className }) => {
-    const buttonClass = classNames('button', className);
+const UserCard: React.FC<UserCardProps> = ({ user, className, liked, disliked, redirect }) => {
+    
     return (
         <div className={CLASSNAME}>
-            <img src={user?.photos[0]} alt={`${user.username}'s profile`}
+            <img  onClick={redirect} src={user?.photos[0]} alt={`${user.username}'s profile`}
                 className={`${CLASSNAME}__photo`} />
             <div className={`${CLASSNAME}__informations`}>
-                <p className={`${CLASSNAME}__informations-username`}>{user.username}, {user.age}</p>
-                <div className={`${CLASSNAME}__informations-location`}>
-                    <img className={`${CLASSNAME}__informations-location-icon`} src='/alternate-map-marker.svg' alt='' />
-                    <p className={`${CLASSNAME}__informations-location-text`}>{user.location}</p>
+                <div className={`${CLASSNAME}__redirect`} onClick={redirect}>
+                    <p className={`${CLASSNAME}__informations-username`}>{user.username}, {user.age}</p>
+                    <div className={`${CLASSNAME}__informations-location`}>
+                        <img className={`${CLASSNAME}__informations-location-icon`} src='/alternate-map-marker.svg' alt='' />
+                        <p className={`${CLASSNAME}__informations-location-text`}>{user.location}</p>
+                    </div>
+                    <p className={`${CLASSNAME}__informations-bio`}>{user.biography}</p>
                 </div>
-                <p className={`${CLASSNAME}__informations-bio`}>{user.biography}</p>
                 <div className={`${CLASSNAME}__interests`}>
                     {user.interests.map((interest, index) => (
                         <span key={index} className={`${CLASSNAME}__tag`}>{interest}</span>
                     ))}
                 </div>
+                    <div className={`${CLASSNAME}__swipe`}>
+                        <div onClick={liked}>
+                            <img className={`${CLASSNAME}__swipe-heart`} src='/like-dark-border.png' alt='' />
+                        </div>
+                        <div onClick={disliked}>
+                            <img className={`${CLASSNAME}__swipe-cross`} src='/cross.png' alt='' />
+                        </div>
+                    </div>
             </div>
         </div>
     );
--- front/src/app/components/homePage/homePage.tsx
+++ front/src/app/components/homePage/homePage.tsx
@@ -13,19 +13,31 @@ const mainPage: React.FC = () => {
     const pathname = usePathname();
     const { user } = useAuth();
 
+    const liked = () => {
+        console.log('LIKED');
+    }
+
+    const dissliked = () => {
+        console.log('DISSLIKED');
+    }
+
+    const redirect  = () => {
+        router.push(`/profile/${user.id}`);
+    }
+
     // [WARNING] Removed for dev mode confort, need to be uncommented
     // useEffect(() => {
     //     if (pathname !== '/login' && !user.jwt_token)
     //         redirectLogin();
     // })
 
-    const redirectLogin = () => {
-        router.push('/login');
-    }
+    // const redirectLogin = () => {
+    //     router.push('/login');
+    // }
 
     return (
         <>
-            <UserCard user={data.user} />
+            <UserCard user={data.user} liked={liked} disliked={dissliked} redirect={redirect} />
         </>
     );
 }
