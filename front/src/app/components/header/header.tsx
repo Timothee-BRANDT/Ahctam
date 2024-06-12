755a31bf7586a11a061c120f68d90ad3c250f837
Timothée BRANDT <timothee@metacard.gift>
2024-06-12 09:15:38 +0200

    responsive header with menu dropdown

--- front/src/app/components/header/header.tsx
+++ front/src/app/components/header/header.tsx
@@ -6,6 +6,15 @@ import Link from 'next/link';
 import "./header.scss"
 import Button from '@/app/components/core/button/button';
 import { useRouter } from 'next/navigation';
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+
 
 const Header: React.FC = () => {
     const router = useRouter();
@@ -16,18 +25,52 @@ const Header: React.FC = () => {
     const redirectProfile = () => {
         router.push('/profile/update');
     }
+    const redirectLikes = () => {
+        router.push('/theyLikesMe');
+    }
+    const redirectSaw = () => {
+        router.push('/theySawMe');
+    }
     const redirectLogin = () => {
         router.push('/login');
     }
+    function toggleMenu() {
+        const navLinks = document.querySelector('.navLinks');
+        if (navLinks) {
+            navLinks.classList.toggle('show');
+        }
+    }
+
     return (
         <header className="header">
             <p>PiggyDate</p>
             <nav>
                 <ul className="navLinks">
                     <Button title="Home" onClick={redirectHome} className="home" />
-                    {user.jwt_token && <Button title="Profile" onClick={redirectProfile} />}
+                    {user.jwt_token && <Button title="They Likes Me" onClick={redirectLikes} />}
+                    {user.jwt_token && <Button title="They Saw Me" onClick={redirectSaw} />}
                     {user.jwt_token && <Button title="Logout" onClick={logout} />}
+                    {user.jwt_token && <Button title="Profile" onClick={redirectProfile} />}
                 </ul>
+                <DropdownMenu>
+                    <DropdownMenuTrigger asChild>
+                        <button className="menuButton" onClick={toggleMenu}>Menu</button>
+                    </DropdownMenuTrigger>
+                    <DropdownMenuContent className="w-56">
+                        <DropdownMenuItem>
+                            {user.jwt_token && <Button title="They Likes Me" onClick={redirectLikes} />}
+                        </DropdownMenuItem>
+                        <DropdownMenuItem>
+                            {user.jwt_token && <Button title="They Saw Me" onClick={redirectSaw} />}
+                        </DropdownMenuItem>
+                        <DropdownMenuItem>
+                            {user.jwt_token && <Button title="Profile" onClick={redirectProfile} />}
+                        </DropdownMenuItem>
+                        <DropdownMenuItem>
+                            {user.jwt_token && <Button title="Logout" onClick={logout} />}
+                        </DropdownMenuItem>
+                    </DropdownMenuContent>
+                </DropdownMenu>
             </nav>
         </header>
     );
