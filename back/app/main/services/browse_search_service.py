810a2bee53da1ab9624588b77de62c1ba7a39970
Darrens24 <edouard.leleux@gmail.com>
2024-09-19 19:27:13 +0200

    browse architecture optimized

--- back/app/main/services/browse_search_service.py
+++ back/app/main/services/browse_search_service.py
@@ -1,43 +1,37 @@
-from logger import logger
-from typing import (
-    Dict,
-    List,
-    Tuple
-)
-from flask import (
-    jsonify,
-    current_app,
-)
 import json
-from psycopg2.extras import RealDictCursor
-from app.main import main
+from typing import Dict, List, Tuple, Any
+##########################################################
 from app.database import get_db_connection
+from app.main import main
 from app.main.services.algo_service import matching_score
 # from app.authentication.views.decorators import jwt_required
+##########################################################
+from flask import current_app, jsonify
+from logger import logger
+from psycopg2.extras import RealDictCursor
 
 
 def get_matching_users(
     user_data: Dict,
     cursor,
-    offset: int,
-    limit: int
-):
+) -> List[Dict]:
     """
     TODO: Now work on the interests brooo
     It will need another join with the user_interest relation table
     """
-    gender_sex_query = """
-    SELECT u.id,
-           u.firstname,
-           u.age,
-           u.biography,
-           u.gender,
-           u.sexual_preferences,
-           u.fame,
-           l.latitude,
-           l.longitude,
-           l.city,
-           STRING_AGG(i.name, ', ') AS interests
+    matchers_query = """
+    SELECT
+        u.id,
+        u.firstname,
+        u.age,
+        u.biography,
+        u.gender,
+        u.sexual_preferences,
+        u.fame,
+        l.latitude,
+        l.longitude,
+        l.city,
+        STRING_AGG(i.name, ', ') AS interests
     FROM users u
     JOIN locations l ON u.id = l.located_user
     LEFT JOIN user_interests ui ON u.id = ui.user_id
@@ -47,14 +41,26 @@ def get_matching_users(
             (%s = 'both' AND (u.sexual_preferences = 'both' OR u.sexual_preferences = %s))
             OR (u.gender = %s AND (u.sexual_preferences = 'both' OR u.sexual_preferences = %s))
           )
-    GROUP BY u.id, u.firstname, u.age, u.biography, u.gender, u.sexual_preferences, u.fame, l.latitude, l.longitude, l.city
+    GROUP BY
+        u.id,
+        u.firstname,
+        u.age,
+        u.biography,
+        u.gender,
+        u.sexual_preferences,
+        u.fame,
+        l.latitude,
+        l.longitude,
+        l.city
     """
 
-    query_params = (user_data['id'],
-                    user_data['sexual_preferences'],
-                    user_data['gender'],
-                    user_data['gender'],
-                    user_data['sexual_preferences'])
+    matchers_query_params = (
+        user_data["id"],
+        user_data["sexual_preferences"],
+        user_data["gender"],
+        user_data["gender"],
+        user_data["sexual_preferences"],
+    )
 
     location_query = """
     SELECT l.latitude AS latitude,
@@ -68,71 +74,83 @@ def get_matching_users(
     GROUP BY l.latitude, l.longitude, l.city
     """
     try:
-        cursor.execute(location_query, (user_data['id'],))
-        location_and_interests = cursor.fetchone()
-        user_data['latitude'] = location_and_interests['latitude']
-        user_data['longitude'] = location_and_interests['longitude']
-        user_data['city'] = location_and_interests['city']
-        user_data['interests']: str = location_and_interests['interests']
+        cursor.execute(location_query, (user_data["id"],))
+        location_and_interests: Dict = dict(cursor.fetchone())
+        user_data["latitude"] = location_and_interests["latitude"]
+        user_data["longitude"] = location_and_interests["longitude"]
+        user_data["city"] = location_and_interests["city"]
+        user_data["interests"] = location_and_interests["interests"]
 
-        cursor.execute(gender_sex_query, query_params)
+        cursor.execute(matchers_query, matchers_query_params)
         matching_users = cursor.fetchall()
 
-        for matching_user in matching_users:
-            matching_user['matching_score'] = matching_score(
+        for index, matching_user in enumerate(matching_users):
+            matching_user["matching_score"] = matching_score(
                 user_data, matching_user)
-            print('user_name', matching_user['firstname'])
-            print('matching_score', matching_user['matching_score'])
+            logger.info("user_name", matching_user["firstname"])
+            logger.info("matching_score", matching_user["matching_score"])
+            # TODO: Remove this break
+            if index == 10:
+                break
 
         matching_users: List = sorted(
-            matching_users, key=lambda x: x['matching_score'], reverse=True)
+            matching_users,
+            key=lambda x: x["matching_score"],
+            reverse=True
+        )
+        return matching_users
 
     except Exception as e:
         raise Exception(str(e))
 
-    return matching_users
 
-
-@main.route('/test-redis', methods=['GET'])
+@main.route("/test-redis", methods=["GET"])
 def test_redis():
-    redis_client = current_app.extensions['redis']
-    redis_client.set('test', 'coucou')
-    test = redis_client.get('test').decode('utf-8')
-    return jsonify({'message': test}), 200
+    redis_client = current_app.extensions["redis"]
+    redis_client.set("test", "coucou")
+    test = redis_client.get("test").decode("utf-8")
+    return jsonify({"message": test}), 200
 
 
 def apply_filters(
-        matching_users: List[Dict],
-        age: int,
-        fame: int,
-        distance: int,
-        common_interests: int):
+    matching_users: List[Dict],
+    age: int,
+    fame: int,
+    distance: int,
+    common_interests: int,
+) -> List[Dict]:
     """
     This function will filter the matching users based on the query parameters
     """
     if age:
         matching_users = [
-            user for user in matching_users if user['age'] == age]
+            user for user in matching_users if user["age"] == age]
     if fame:
         matching_users = [
-            user for user in matching_users if user['fame'] == fame]
+            user for user in matching_users if user["fame"] == fame]
     if distance:
         matching_users = [
-            user for user in matching_users if user['distance'] == distance]
+            user for user in matching_users if user["distance"] == distance
+        ]
     if common_interests:
         matching_users = [
-            user for user in matching_users if user['common_interests'] == common_interests]
+            user
+            for user in matching_users
+            if user["common_interests"] == common_interests
+        ]
+
+    return matching_users
 
 
 def perform_browsing(
     filters: bool,
     user_id: int,
-    age: int = None,
-    fame: int = None,
-    distance: int = None,
-    common_interests: int = None,
-    offset: int = 0,
-    limit: int = 9
+    age: int,
+    fame: int,
+    distance: int,
+    common_interests: int,
+    offset: int,
+    limit: int,
 ) -> Tuple[Dict, int]:
     """
     Actually it will compute the algorithm every time a user wants to browse
@@ -141,10 +159,11 @@ def perform_browsing(
     """
     conn = get_db_connection()
     cur = conn.cursor(cursor_factory=RealDictCursor)
-    redis_client = current_app.extensions['redis']
+    redis_client = current_app.extensions["redis"]
     try:
         user_query = """
-SELECT id,\
+SELECT \
+id,\
 firstname,\
 lastname,\
 age,\
@@ -157,36 +176,37 @@ FROM users
 WHERE id = %s
         """
         redis_key: str = f"matching:{user_id}"
-        matching_users: List = []
+        matching_users: List[Dict] = []
         if not redis_client.exists(redis_key):
-            print('nothing in redis yet')
+            logger.info("nothing in redis yet")
             cur.execute(user_query, (user_id,))
-            user_data: Dict = dict(cur.fetchone())
-            print(f'{user_data=}')
+            user_data: Dict[str, Any] = dict(cur.fetchone())
+            logger.info(f"from db:{user_data=}")
             matching_users = get_matching_users(
                 user_data=user_data,
                 cursor=cur,
-                offset=offset,
-                limit=limit
             )
-            print('we received the matching users: ', matching_users)
+            logger.info("get_matching_users:", matching_users)
             redis_client.set(redis_key, json.dumps(matching_users), ex=3600)
         else:
-            print('something in redis')
+            logger.info("something in redis")
             matching_users = json.loads(
-                redis_client.get(redis_key).decode('utf-8'))
-            print('in redis the matching users: ', matching_users)
-        matching_users = apply_filters(
-            matching_users,
-            age,
-            fame,
-            distance,
-            common_interests
-        )
-        return {'users': matching_users[offset:limit]}, 200
+                redis_client.get(redis_key).decode("utf-8")
+            )
+            logger.info("matching_users in redis: ", matching_users)
+
+        if filters:
+            matching_users = apply_filters(
+                matching_users=matching_users,
+                age=age,
+                fame=fame,
+                distance=distance,
+                common_interests=common_interests
+            )
+        return {"users": matching_users[offset:limit]}, 200
 
     except Exception as e:
-        return {'error': str(e)}, 400
+        return {"error": str(e)}, 400
     finally:
         cur.close()
         conn.close()
--- back/app/main/views/browse.py
+++ back/app/main/views/browse.py
@@ -1,43 +1,36 @@
-from typing import Dict, Any
-from flask import (
-    request,
-    jsonify,
-    current_app,
-    Response
-)
+from typing import Any, Dict, Tuple
+
 import jwt
-from app.main import main
 from app.authentication.views.decorators import jwt_required
+from app.main import main
 from app.main.services.browse_search_service import perform_browsing
+from flask import Response, current_app, jsonify, request
 from logger import logger
 
 
-@main.route('/browse', methods=['GET'])
+@main.route("/browse", methods=["GET"])
 @jwt_required
-def browse() -> Response:
+def browse() -> Tuple[Response, int]:
     """
     Returns: a Response with an array of user objects
     Only what is necessary for the card display
     """
     try:
-        filter_args = ['age', 'fame', 'distance', 'tags']
-        filters: bool = False
-        if any(arg in request.args for arg in filter_args):
-            logger.info('Filtering')
-            filters = True
+        filter_args = ["age", "fame", "distance", "tags"]
+        filters: bool = True if any(
+            arg in request.args for arg in filter_args) else False
 
-        age: int = int(request.args.get('age', 0))
-        fame: int = int(request.args.get('fame', 0))
-        distance: int = int(request.args.get('distance', 0))
-        common_interests: int = int(request.args.get('tags', 0))
-        offset: int = int(request.args.get('offset', 0))
-        limit: int = int(request.args.get('limit', 9))
-        logger.info(f'Offset: {offset}')
-        logger.info(f'Limit: {limit}')
-        token: str = request.headers.get('Authorization', '').split(' ')[1]
+        age: int = int(request.args.get("age", 0))
+        fame: int = int(request.args.get("fame", 0))
+        distance: int = int(request.args.get("distance", 0))
+        common_interests: int = int(request.args.get("tags", 0))
+        offset: int = int(request.args.get("offset", 0))
+        limit: int = int(request.args.get("limit", 9))
+        token: str = request.headers.get("Authorization", "").split(" ")[1]
         user: Dict[str, Any] = jwt.decode(
-            token, current_app.config['SECRET_KEY'], algorithms=['HS256'])
-        user_id = user['id']
+            token, current_app.config["SECRET_KEY"], algorithms=["HS256"]
+        )
+        user_id: int = int(user["id"])
         response_dict: Dict
         status_code: int
         response_dict, status_code = perform_browsing(
@@ -48,10 +41,10 @@ def browse() -> Response:
             distance=distance,
             common_interests=common_interests,
             offset=offset,
-            limit=limit
+            limit=limit,
         )
         return jsonify(response_dict), status_code
 
     except Exception as e:
-        logger.error(f'Error: {e}')
-        return jsonify({'message': 'Error'}), 500
+        logger.error(f"Error: {e}")
+        return jsonify({"message": "Error"}), 500
