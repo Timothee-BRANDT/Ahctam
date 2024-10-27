19afda311747152977b7ede23e4ab9157baf09f8
Darrens24 <edouard.leleux@gmail.com>
2024-10-27 11:13:23 +0100

    history and search refreshed

--- front/src/app/components/history/historyPage.tsx
+++ front/src/app/components/history/historyPage.tsx
@@ -6,72 +6,76 @@ import { User } from "@/app/types";
 import { useEffect, useState } from "react";
 import { UserCard } from "@/components/ui/carousel";
 import { serverIP } from "@/app/constants";
+import createRefreshClosure from "@/app/constants";
 
 import "./historyPage.scss";
 
 const CLASSNAME = "browse";
 
 const historyPage: React.FC = () => {
-    const router = useRouter();
-    const [isLoggedIn, setIsLoggedIn] = useState(false);
-    const [profiles, setProfiles] = useState<User[]>([]);
+  const router = useRouter();
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [profiles, setProfiles] = useState<User[]>([]);
+  const { isJwtInCookie } = useAuth();
 
-    const { isJwtInCookie, getCookie } = useAuth();
+  const redirectLogin = () => {
+    router.push("/login");
+  };
 
-    const redirectLogin = () => {
-        router.push("/login");
-    };
+  useEffect(() => {
+    if (!isJwtInCookie()) {
+      redirectLogin();
+    }
+    getProfiles();
+    setIsLoggedIn(isJwtInCookie());
+  }, []);
 
-    useEffect(() => {
-        if (!isJwtInCookie()) {
-            redirectLogin();
-        }
-        getProfiles();
-        setIsLoggedIn(isJwtInCookie());
-    }, []);
-    // WARNING: I put [] because [profiles] was causing an infinite loop]
+  const getProfiles = async () => {
+    try {
+      const refreshClosure = createRefreshClosure();
+      const url = `http://${serverIP}:5000/api/getMyViews`;
+      const infos = {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      };
+      const response = await refreshClosure(url, infos);
+      if (!response.ok) {
+        throw new Error("Error fetching profiles");
+      }
+      const data = await response.json();
+      setProfiles(data);
+    } catch (error) {
+      console.error("Error fetching profiles:", error);
+    }
+  };
 
-    const getProfiles = async () => {
-        const token = getCookie("jwt_token");
-        const response = await fetch(`http://${serverIP}:5000/api/getMyViews`, {
-            method: "GET",
-            credentials: "include",
-            headers: {
-                "Content-Type": "application/json",
-                Authorization: `Bearer ${token}`,
-            },
-        });
-        const data = await response.json();
-        if (response.ok) {
-            setProfiles(data);
-        }
-    };
+  const redirect = (id: number) => {
+    router.push(`/profile/${id}`);
+  };
 
-    const redirect = (id: number) => {
-        router.push(`/profile/${id}`);
-    };
-
-    return (
+  return (
+    <>
+      {isLoggedIn && (
         <>
-            {isLoggedIn && (
-                <>
-                    <div className={CLASSNAME}>
-                        <div className={`${CLASSNAME}__list`}>
-                            {profiles.map((profile, index) => {
-                                return (
-                                    <UserCard
-                                        key={index}
-                                        user={profile}
-                                        redirect={() => redirect(profile.id)}
-                                    />
-                                );
-                            })}
-                        </div>
-                    </div>
-                </>
-            )}
+          <div className={CLASSNAME}>
+            <div className={`${CLASSNAME}__list`}>
+              {profiles.map((profile, index) => {
+                return (
+                  <UserCard
+                    key={index}
+                    user={profile}
+                    redirect={() => redirect(profile.id)}
+                  />
+                );
+              })}
+            </div>
+          </div>
         </>
-    );
+      )}
+    </>
+  );
 };
 
 export default historyPage;
--- front/src/app/components/searchPage/searchPage.tsx
+++ front/src/app/components/searchPage/searchPage.tsx
@@ -1,22 +1,19 @@
 "use client";
 
-import { useRouter, usePathname } from "next/navigation";
+import { useRouter } from "next/navigation";
 import { useAuth } from "@/app/authContext";
 import { User } from "@/app/types";
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 import { UserCard } from "@/components/ui/carousel";
 import { Button } from "@/components/ui/button";
-
-import { DropdownMenuCheckboxItemProps } from "@radix-ui/react-dropdown-menu";
 import { ChevronLeft, ChevronRight } from "lucide-react";
 
 import "./searchPage.scss";
 import { Slider } from "@/components/ui/slider";
 import { serverIP } from "@/app/constants";
+import createRefreshClosure from "@/app/constants";
 
 const CLASSNAME = "browse";
-
-type Checked = DropdownMenuCheckboxItemProps["checked"];
 
 const searchPage: React.FC = () => {
   const [age, setAge] = useState(0);
@@ -26,14 +23,13 @@ const searchPage: React.FC = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [profiles, setProfiles] = useState<User[]>([]);
   const [responsiveFilterButton, setResponsiveFilterButton] = useState(false);
-  const [isFetched, setIsFetched] = useState(false);
   const [offset, setOffset] = useState(0);
   const [totalProfiles, setTotalProfiles] = useState(0);
   const limit = 8;
 
   const router = useRouter();
 
-  const { isJwtInCookie, getCookie } = useAuth();
+  const { isJwtInCookie } = useAuth();
 
   const redirectLogin = () => {
     router.push("/login");
@@ -45,17 +41,16 @@ const searchPage: React.FC = () => {
     getProfiles();
     setIsLoggedIn(isJwtInCookie());
   }, [offset]);
-  // WARNING: only 1 display "[]" because infinite loop with [profiles]
-  // I replaced it with offset to rerender every pagination changes
 
   const redirect = (id: number) => {
     router.push(`/profile/${id}`);
   };
 
   const getProfiles = async () => {
-    const cookie = getCookie("jwt_token");
-    const response = await fetch(
-      `http://${serverIP}:5000/browse?` +
+    try {
+      const refreshClosure = createRefreshClosure();
+      const browseURL =
+        `http://${serverIP}:5000/browse?` +
         new URLSearchParams({
           age: String(age),
           fame: String(fame),
@@ -63,21 +58,24 @@ const searchPage: React.FC = () => {
           tags: String(tags),
           offset: offset.toString(),
           limit: (offset + limit).toString(),
-        }),
-      {
+        });
+      const browseOptions = {
         method: "GET",
-        credentials: "include",
         headers: {
           "Content-Type": "application/json",
-          Authorization: `Bearer ${cookie}`,
         },
-      },
-    );
-    const data_response = await response.json();
-    if (response.ok) {
-      console.log("From browse:", data_response);
+      };
+
+      const response = await refreshClosure(browseURL, browseOptions);
+      if (!response.ok) {
+        throw new Error("Error fetching profiles");
+      }
+      const data_response = await response.json();
+
       setProfiles(data_response.users);
       setTotalProfiles(data_response.total);
+    } catch (error) {
+      console.error("Error fetching profiles:", error);
     }
   };
 
